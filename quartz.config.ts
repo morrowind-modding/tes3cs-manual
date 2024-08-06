b866266551a8eda9b598634d6b59c938887c61c0
MasssiveJuice <nicholas.h.rowell@gmail.com>
2024-08-06 19:31:19 +1200

    Fix: Shorten page title to 'TES3CS Docs'

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "TES3 Construction Set Docs",
+    pageTitle: "TES3CS Docs",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
