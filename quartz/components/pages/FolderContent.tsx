d2435ebbfc6b31f15937a92f28e830481952344f
MasssiveJuice <nicholas.h.rowell@gmail.com>
2024-08-28 22:08:13 +1200

    Add `<hr>` before page-listing

--- quartz/components/pages/FolderContent.tsx
+++ quartz/components/pages/FolderContent.tsx
@@ -50,6 +50,7 @@ export default ((opts?: Partial<FolderContentOptions>) => {
     return (
       <div class={classes}>
         <article>{content}</article>
+        <hr></hr>
         <div class="page-listing">
           {options.showFolderCount && (
             <h2 class="foldercount-heading">
